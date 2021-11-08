f9f65eb3ad55ffbf15025d43388cbc6e42196616
Matsuuu <huhta.matias@gmail.com>
2021-11-08 23:31:32 +0200

    Some real basic dataset support

--- demo/src/DemoProject.js
+++ demo/src/DemoProject.js
@@ -17,6 +17,7 @@ export class DemoProject extends LitElement {
             title: { type: String },
             counter: { type: Number },
             chartData: { type: Array },
+            chartDataTwo: { type: Array },
         };
     }
 
@@ -29,6 +30,18 @@ export class DemoProject extends LitElement {
             { label: 'Bar', data: 15 },
             { label: 'Baz', data: 10 },
         ];
+        this.chartData = [
+            [
+                { label: 'Foo', data: 12 },
+                { label: 'Bar', data: 15 },
+                { label: 'Baz', data: 10 },
+            ],
+            [
+                { label: 'Foo', data: 8 },
+                { label: 'Bar', data: 14 },
+                { label: 'Baz', data: 11 },
+            ],
+        ];
     }
 
     __increment() {
@@ -36,7 +49,21 @@ export class DemoProject extends LitElement {
     }
 
     addData() {
-        this.chartData = [...this.chartData, { label: "New", data: Math.floor(Math.random() * 15 + 5) }]
+        this.chartData = this.chartData.map(cd => {
+            return [...cd, this.rand()];
+        });
+    }
+
+    addDataset() {
+        const data = [];
+        for (let i = 0; i < this.chartData[0].length; i++) {
+            data.push(this.rand());
+        }
+        this.chartData = [...this.chartData, data];
+    }
+
+    rand() {
+        return { label: 'New', data: Math.floor(Math.random() * 15 + 5) };
     }
 
     render() {
@@ -44,14 +71,24 @@ export class DemoProject extends LitElement {
             <h2>${this.title} Nr. ${this.counter}!</h2>
             <button @click=${this.__increment}>increment</button>
             <button @click=${this.addData}>Add data</button>
+            <button @click=${this.addDataset}>Add dataset</button>
 
-            <chart-js>
+            <chart-js label="Random data">
                 ${this.chartData.map(
-            d => html`
-                        <chart-js-data
-                            label=${d.label}
-                            data=${d.data}
-                        ></chart-js-data>
+            (cd, i) => html`
+                        <chart-js-dataset label="Data ${i}">
+                            ${cd.map(d => html` <chart-js-data label=${d.label} data=${d.data}></chart-js-data> `)}
+                        </chart-js-dataset>
+                    `,
+        )}
+            </chart-js>
+
+            <chart-js type="line">
+                ${this.chartData.map(
+            (cd, i) => html`
+                        <chart-js-dataset label="Data ${i}">
+                            ${cd.map(d => html` <chart-js-data label=${d.label} data=${d.data}></chart-js-data> `)}
+                        </chart-js-dataset>
                     `,
         )}
             </chart-js>
