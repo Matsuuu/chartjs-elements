7aeb3dd30b4b274a7cb345c2c9e17e45206bf093
Matsuuu <huhta.matias@gmail.com>
2021-11-12 11:09:05 +0200

    Bug fixing all along

--- example/src/ExamplePlayground.js
+++ example/src/ExamplePlayground.js
@@ -30,26 +30,31 @@ export class ExamplePlayground extends LitElement {
         super();
     }
 
-    render() {
+    getBarChartPlayground() {
         return html`
-            <main>
                 <playground-ide editable-file-system line-numbers resizable>
                     <script type="sample/html" filename="index.html">
+<!-- playground-hide -->
 <!doctype html>
 <body>
-  <chart-js type="bar">
+<!-- playground-hide-end -->
+  <chart-js type="bar" >
     <chart-js-title text="Data chart #1" size="24" padding="5"></chart-js-title>
     <chart-js-title text="This is a data chart" size="18" padding="0" subtitle></chart-js-title>
     <chart-js-legend align="start"></chart-js-legend>
+
     <chart-js-dataset label="Data">
-        <chart-js-data label="Foo" data="10"></chart-js-data>
-        <chart-js-data label="Bar" data="15"></chart-js-data>
-        <chart-js-data label="Baz" data="8"></chart-js-data>
+        <chart-js-data label="HTML" data="15"></chart-js-data>
+        <chart-js-data label="Javascript" data="10"></chart-js-data>
+        <chart-js-data label="CSS" data="8"></chart-js-data>
     </chart-js-dataset>
+
   </chart-js>
 
+<!-- playground-hide -->
   <script type="module" src="./imports.js">&lt;/script>
 </body>
+<!-- playground-hide-end -->
                     </script>
 
                     <script type="sample/js" filename="imports.js">
@@ -58,13 +63,101 @@ export class ExamplePlayground extends LitElement {
                     <script type="sample/json" filename="package.json">
 {
   "dependencies": {
-    "chartjs-elements": "0.0.1-1",
+    "chartjs-elements": "0.0.1-4",
     "chart.js": "3.6.0"
   }
 }
 
                     </script>
                 </playground-ide>
+        `
+    }
+
+    getPieChartPlayground() {
+        return html`
+                <playground-ide editable-file-system line-numbers resizable>
+                    <script type="sample/html" filename="index.html">
+<!-- playground-hide -->
+<!doctype html>
+<body>
+<!-- playground-hide-end -->
+  <chart-js type="pie" >
+    <chart-js-title text="Data chart #1" size="24" padding="5"></chart-js-title>
+    <chart-js-title text="This is a data chart" size="18" padding="0" subtitle></chart-js-title>
+    <chart-js-legend align="start"></chart-js-legend>
+
+    <chart-js-dataset label="Data">
+        <chart-js-data label="Foo" data="10"></chart-js-data>
+        <chart-js-data label="Bar" data="15"></chart-js-data>
+        <chart-js-data label="Baz" data="8"></chart-js-data>
+    </chart-js-dataset>
+
+  </chart-js>
+
+<!-- playground-hide -->
+  <script type="module" src="./imports.js">&lt;/script>
+</body>
+<!-- playground-hide-end -->
+                    </script>
+
+                    <script type="sample/js" filename="imports.js">
+                        import "chartjs-elements";
+                    </script>
+                    <script type="sample/json" filename="package.json">
+{
+  "dependencies": {
+    "chartjs-elements": "0.0.1-4",
+    "chart.js": "3.6.0"
+  }
+}
+
+                    </script>
+                </playground-ide>
+        `
+    }
+
+    getMinimalChartPlayground() {
+        return html`
+                <playground-ide editable-file-system line-numbers resizable>
+                    <script type="sample/html" filename="index.html">
+<!-- playground-hide -->
+<!doctype html>
+<body>
+<!-- playground-hide-end -->
+  <chart-js type="pie">
+    <chart-js-data label="Foo" data="10"></chart-js-data>
+    <chart-js-data label="Bar" data="15"></chart-js-data>
+    <chart-js-data label="Baz" data="8"></chart-js-data>
+  </chart-js>
+
+<!-- playground-hide -->
+  <script type="module" src="./imports.js">&lt;/script>
+</body>
+<!-- playground-hide-end -->
+                    </script>
+
+                    <script type="sample/js" filename="imports.js">
+                        import "chartjs-elements";
+                    </script>
+                    <script type="sample/json" filename="package.json">
+{
+  "dependencies": {
+    "chartjs-elements": "0.0.1-4",
+    "chart.js": "3.6.0"
+  }
+}
+
+                    </script>
+                </playground-ide>
+    `
+    }
+
+    render() {
+        return html`
+            <main>
+                ${this.getBarChartPlayground()}
+                ${this.getPieChartPlayground()}
+                ${this.getMinimalChartPlayground()}
             </main>
         `;
     }
